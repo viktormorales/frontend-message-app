8e4e6e3ca1470b0e08d921d9c0ab0c1b61dd9f12
viktormorales <viktorhugomorales@gmail.com>
2021-01-26 23:58:38 -0300

    Nuevo endpoint: messages

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 
+// Ruta de /api/config
 const config = require('./config');
 router.use('/config', config);
+
+// Ruta de /api/messages
+const messages = require('./messages');
+router.use('/messages', messages);
 
 router.get('/', function (req, res) {
     res.status(200).json({ message: 'Estás conectado a nuestra API' })
