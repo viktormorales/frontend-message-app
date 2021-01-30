2166324ebccc8b29190b00b5b54a6692bc4f4139
viktormorales <viktorhugomorales@gmail.com>
2021-01-29 23:30:32 -0300

    Empezando con mongoose

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const port = 3000;
+
+//Connect to the MongoDB test database  
+mongoose.connect('mongodb://localhost/test_database');
 
 // Expone la ruta /api con los archivos dentro de la carpeta /server/api
 const api = require("./server/api");
@@ -10,5 +14,5 @@ app.use("/api", api);
 app.use("/", express.static('client'));
 
 app.listen(port, () => {
- console.log(`El servidor está inicializado en el puerto ${port}`);
+    console.log(`El servidor está inicializado en el puerto ${port}`);
 });
