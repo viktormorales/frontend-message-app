a2dfc5dd3dde8c760177bdb77ff9d8768ebe8beb
viktormorales <viktorhugomorales@gmail.com>
2021-01-29 23:34:06 -0300

    Nuevas rutas

--- server/api/index.js
+++ server/api/index.js
@@ -1,14 +1,20 @@
 const router = require('express').Router();
+const mongo = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
 
 // Ruta de /api/config
 const config = require('./config');
 router.use('/config', config);
 
+// Ruta de /api/agencies
+const agencies = require('./agencies');
+router.use('/agencies', agencies);
+
 // Ruta de /api/messages
 const messages = require('./messages');
 router.use('/messages', messages);
 
-router.get('/', function (req, res) {
+router.get('/', (req, res) => {
     res.status(200).json({ message: 'Estás conectado a nuestra API' })
 });
 
